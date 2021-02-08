eda0b709ae75f4fac72c06f9d045aa52f3e80469
maxbesner <max_besner@hotmail.com>
2021-02-07 21:33:59 -0500

    Started working on unit testing

--- app/src/main/java/com/example/pages/MainActivity.java
+++ app/src/main/java/com/example/pages/MainActivity.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 
 import com.example.myapplication.R;
 
+import java.util.Set;
+
 import entities.User;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Converts User Object data to a String Set by passing the data to a method that adds
+    //String Sets to Shared Preferences
     public void addUserToSharedPreferences(User user){
+        addStringSetToShardPreferences("userData", user.getUserData());
+    }
+
+    //Adds any String Set to SharedPreferences so it can be accessed in a different page
+    //This is the best way I can think of to send object data from page to page
+    public void addStringSetToShardPreferences(String key, Set<String> set){
         SharedPreferences sharedPref = getSharedPreferences("MainActivity.java", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
 
-        editor.putStringSet("userData", user.getUserData());
+        editor.putStringSet(key, set);
         editor.apply();
     }
 
