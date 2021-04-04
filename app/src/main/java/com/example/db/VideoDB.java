43c2209e2bf77abde491ef5d0e311c218b27a17e
joseAdelin <bodj0001@algonquinlive.com>
2021-04-04 19:25:39 -0400

    videos load dynamically

---
+++ app/src/main/java/com/example/db/VideoDB.java
@@ -0,0 +1,113 @@
+package com.example.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.example.model.Video;
+import com.example.tools.PasswordUtilities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoDB extends SQLiteOpenHelper {
+    public static final String DBNAME = "video.db";
+    SQLiteDatabase MyDB;
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String COL_ID = "id";
+    private static final String COL_TITLE= "title";
+    private static final String COL_URL= "url";
+    private static final String COL_THUMBNAIL= "thumbnail";
+
+    public VideoDB(@Nullable Context context) {
+        super(context, DBNAME, null, DATABASE_VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase MyDB) {
+        MyDB.execSQL("drop table if exists users");
+        MyDB.execSQL("create table FavVideos(id TEXT primary key,title TEXT, url TEXT, thumbnail TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase MyDB, int oldVersion, int newVersion) {
+        MyDB.execSQL("drop table if exists FavVideos");
+    }
+
+    public boolean insertData(String id,String title, String url, String thumbnail){
+
+        MyDB = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COL_ID,id);
+        values.put(COL_TITLE, title);
+        values.put(COL_URL, url);
+        values.put(COL_THUMBNAIL,thumbnail);
+
+        long r=MyDB.insert("FavVideos", null, values);
+
+        return r != -1;
+    }
+
+    public boolean delete(String id){
+        MyDB = this.getWritableDatabase();
+
+       return 0!=MyDB.delete("FavVideos","id=?",new String[]{id});
+    }
+
+    public boolean exists(String id) {
+        MyDB = this.getWritableDatabase();
+
+        String Query = "Select * from FavVideos where id = '" + id+"'";
+        Cursor cursor = MyDB.rawQuery(Query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
+    public List<Video> getAllFavVideos(){
+        ArrayList<Video> videos = new ArrayList<Video>();
+        try {
+            SQLiteDatabase db = getWritableDatabase();
+            Cursor c = null;
+            c = db.rawQuery("SELECT * FROM FavVideos", null);
+
+            System.out.println(c);
+
+            for (c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+
+                String id = c.getString(c.getColumnIndex(COL_ID));
+                String title = c.getString(c.getColumnIndex(COL_TITLE));
+                String url = c.getString(c.getColumnIndex(COL_URL));
+                String thumbnail = c.getString(c.getColumnIndex(COL_THUMBNAIL));
+
+                Video video = new Video();
+                video.setId(id);
+                video.setTitle(title);
+                video.setVideoURL(url);
+                video.setThumbnail(thumbnail);
+
+                videos.add(video);
+            }
+            c.close();
+            db.close();
+
+        } catch (Exception e) {
+            Log.e("something went wrong", "" + e);
+        }
+
+        return videos;
+    }
+
+}
+
--- app/src/main/java/com/example/fragments/FavoritesFragment.java
+++ app/src/main/java/com/example/fragments/FavoritesFragment.java
@@ -4,17 +4,77 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.db.VideoDB;
+import com.example.model.Video;
 import com.example.myapplication.R;
+import com.example.pages.MainActivity;
+import com.example.tools.VideoViewAdapter;
+import com.example.tools.VideoViewListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
-public class FavoritesFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FavoritesFragment extends Fragment implements VideoViewListener {
+
+    private RecyclerView vView;
+    private VideoViewAdapter vAdapter;
+    private RecyclerView.LayoutManager vManager;
+    private List<Video> videos;
+    private VideoDB videoDB;
+    private View view;
 
     public FavoritesFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        view = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+
+        vView = (RecyclerView) view.findViewById(R.id.recycler_video);
+
+        vManager = new GridLayoutManager(getActivity(), 2);
+        vView.setLayoutManager(vManager);
+
+
+        videoDB = new VideoDB(getActivity());
+
+        loadVideos();
+
+        return view;
+    }
+
+    public void loadVideos() {
+
+        videos = new ArrayList<>();
+
+        videos = videoDB.getAllFavVideos();
+
+        if (videos==null || videos.isEmpty()) {
+            Toast.makeText(getActivity(), "There's nothing in favorites for now!", Toast.LENGTH_SHORT).show();
+        }else{
+            loadVideosAdapter(videos);
+        }
+    }
+
+    private void loadVideosAdapter(List<Video> videos) {
+        vAdapter = new VideoViewAdapter(getContext(),videos);
+        vAdapter.videoViewListener = this;
+
+        vView.setAdapter(vAdapter);
+    }
+
+    @Override
+    public void onVideoClick(Video video) {
+        WatchVideoFragment fragment = WatchVideoFragment.newInstance(video);
+        ((MainActivity)getActivity()).loadFragment(fragment);
     }
 }
